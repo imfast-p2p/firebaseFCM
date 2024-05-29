0406e358040cb6934c2fdcd3e8862708f45296af
sumanmc <105421349+sumanmc@users.noreply.github.com>
2024-05-29 16:03:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import router from './Routes/Notification.js';
+import cors from 'cors';
+
 const app=express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/notification',router);
 const PORT=process.env.PORT||5000
 
-app.listen(PORT,()=>console.log('server started'))
+app.listen(PORT,()=>console.log('server started on port 5000',PORT))
